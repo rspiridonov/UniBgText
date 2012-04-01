3120437de1c9ed6487863c7202f6585ddd6bf762
evilexp <evilexp@gmail.com>
2012-04-01 23:02:51 +0400

    Code refactoring

--- js/break_line.js
+++ js/break_line.js
@@ -1,53 +1,52 @@
-(function($){
+(function ($) {
   $.fn.extend({
     breakLine: function(options) {
       var defaults = {
+        padding: 0,
         tag: 'span'
-      }
+      };
 
       options = $.extend(defaults, options);
 
       return this.each(function() {
-        var o = options, width = $(this).width(), temp_id = 'BreakLineTemp', break_line_array,
-            temp_block_height, temp_text, break_text;
-
-        var virtual_text_block = '<div id="' + temp_id + '"></div>';
+        var o = options, width = $(this).width(), open_tag = '<' + o.tag + '>',
+            close_tag = '</' + o.tag + '>', temp_id = 'BreakLineTemp', 
+            temp_div_selector = '#' + temp_id, 
+            virtual_text_block = '<div id="' + temp_id + '"></div>';
 
         $('body').append(virtual_text_block);
 
-        $('#' + temp_id).css({
+        $(temp_div_selector).css({
           position: 'absolute',
           visibility: 'hidden',
-          width: width - 16
+          width: width - o.padding
         });
 
         $(this).find('p').each(function() {
-          break_line_array = {};
-          temp_block_height = 0;
-          temp_text = $(this).text();
-
-          break_text = '<span>';
+          var temp_block_height = 0, temp_text = $(this).text(), 
+              break_text = open_tag;
+              
           $.each(temp_text.split(' '), function(i, value) {
-            $('#' + temp_id).append(value + ' ');
+            $(temp_div_selector).append(value + ' ');
             
             if (value) {
-              if (temp_block_height != $('#' + temp_id).height()) {
+              if (temp_block_height !== $(temp_div_selector).height()) {
                 if (temp_block_height > 0) {
-                  break_text += '</span><span>';
+                  break_text += close_tag + open_tag;
                 } 
-                temp_block_height = $('#' + temp_id).height();
+                temp_block_height = $(temp_div_selector).height();
               }
             }
             break_text += value + ' ';
           });
-          break_text += '</span>';
+          break_text += close_tag;
           
           
           $(this).html(break_text);
-          $('#' + temp_id).text('');
+          $(temp_div_selector).text('');
         });
         
-        $('#' + temp_id).remove();
+        $(temp_div_selector).remove();
         
         $(this).css('display', 'block');
       });
